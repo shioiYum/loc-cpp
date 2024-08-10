49fefd0801fecfa123ef6c059c5bc9e92b10d715
Saiyam Chettri <saiyum303@gmail.com>
2024-08-10 11:54:02 +0530

    Delete to start new

--- src/Expr.cpp
+++ src/Expr.cpp
@@ -1,15 +1,3 @@
 #include "Expr.hpp"
 #include "Literal.hpp"
 #include "Token.hpp"
-
-Binary::Binary(Expr left, Token oper, Expr right): 
-left(left), oper(oper), right(right) { }
-
-Grouping::Grouping(Expr expression): 
-expression(expression) { }
-
-Literal::Literal(LiteralData value) : value(value) {}
-
-Unary::Unary(Token oper, Expr* right): 
-oper(oper), right(right) { }
-
